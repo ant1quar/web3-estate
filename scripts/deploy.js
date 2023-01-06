5120e8d7d81bff423f69b492073cd08cad9d0541
Иван Кривонос <krivonos.ivann@gmail.com>
2023-01-07 02:08:08 +0300

    contracts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,10 +4,62 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
+const { ethers } = require("hardhat");
 const hre = require("hardhat");
 
-async function main() {
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
+async function main() {
+  const [buyer, seller, inspector, lender] = await ethers.getSigners();
+  const RealEstate = await ethers.getContractFactory("RealEstate");
+  const realEstate = await RealEstate.deploy();
+  await realEstate.deployed();
+  console.log(`Deployed Real Estate Contract at: ${realEstate.address}`);
+  console.log("Minting 3 properties...\n");
+  const nftCount = 3;
+  for (let i = 0; i < nftCount; i++) {
+    const transaction = await realEstate
+      .connect(seller)
+      .mint(
+        `"https://ipfs.io/ipfs/QmQUozrHLAusXDxrvsESJ3PYB3rUeUuBAvVWw6nop2uu7c/${
+          i + 1
+        }.png"`
+      );
+    await transaction.wait();
+  }
+  const Escrow = await ethers.getContractFactory("Escrow");
+  const escrow = await Escrow.deploy(
+    realEstate.address,
+    seller.address,
+    inspector.address,
+    lender.address
+  );
+  await escrow.deployed();
+
+  for (let i = 0; i < nftCount; i++) {
+    const transaction = await realEstate
+      .connect(seller)
+      .approve(escrow.address, i + 1);
+    await transaction.wait();
+  }
+  let transaction = await escrow
+    .connect(seller)
+    .list(1, buyer.address, tokens(20), tokens(10));
+  await transaction.wait();
+
+  transaction = await escrow
+    .connect(seller)
+    .list(2, buyer.address, tokens(15), tokens(5));
+  await transaction.wait();
+
+  transaction = await escrow
+    .connect(seller)
+    .list(3, buyer.address, tokens(10), tokens(5));
+  await transaction.wait();
+
+  console.log("Finished.");
 }
 
 // We recommend this pattern to be able to use async/await everywhere
